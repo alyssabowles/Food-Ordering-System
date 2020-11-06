5a7f3a5ef53440a57ca6b1337baf17f689534b94
Alyssa Bowles <alyssa.s.bowles@gmail.com>
2020-11-05 20:29:30 -0600

    added executeQuery function

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
@@ -14,15 +16,16 @@ public class Main {
 		String url = "jdbc:mariadb://localhost:3306/foodordering";
 		String user;
         String pwd;
+        String ans;
         Statement statement;
         Scanner scan = new Scanner(System.in);
         		
 		try {
 			
 	        System.out.println("Opening a Connection to the Database");
-	        System.out.print("Enter Username:  ");
+	        System.out.print("Enter Username: ");
 	        user = scan.nextLine();
-	        System.out.print("Enter Password:  ");
+	        System.out.print("Enter Password: ");
 	        pwd = scan.nextLine();
 	        System.out.println();
 
@@ -33,12 +36,59 @@ public class Main {
                 System.out.println("Successfully connected.");
                 System.out.println();
             }
+	        
+	        boolean continueQuery = true;
+			while(continueQuery) {
+				executeQuery(scan, statement);
+				System.out.print("Would you like to enter another query? [y/n]  ");
+				ans = scan.nextLine();
+				if (ans.equalsIgnoreCase("n")) {
+					System.out.println("Program Terminated.");
+					continueQuery = false;
+				}
+			}
+	        
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		
 		
-
+	}
+	
+	private static void executeQuery(Scanner scan, Statement statement) {
+		System.out.print("Enter SQL command: ");
+		String query = scan.nextLine();
+		System.out.println();
+		try {
+			boolean b = statement.execute(query);
+			if(b) {
+				ResultSet rs = statement.getResultSet();
+				ResultSetMetaData rsmd = rs.getMetaData();
+				int col = rsmd.getColumnCount();
+				while(rs.next()) {
+					for (int i = 0; i < col; i++) {
+						if (i > 0) {
+							System.out.print(", ");
+						}
+						System.out.print(rs.getString(i+1));
+					}
+					System.out.println("");
+				}
+				System.out.println("");
+			}
+			else {
+				int count = statement.getUpdateCount();
+				if (count > 0) {
+					System.out.println(count + " rows affected");
+				}
+				else {
+					System.out.println("No rows affected");
+				}
+			}
+		}
+		catch(Exception e) {
+			System.out.println("Could not execute query");
+		}
 	}
 
 }
